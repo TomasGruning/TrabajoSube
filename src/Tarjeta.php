0acd328ff5d395d6f675cef4ef71f8add0a24647
TomasGruning <tomasgruning@gmail.com>
2023-08-22 17:46:55 -0300

    recargarSueldo

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -7,6 +7,7 @@ class Tarjeta
     public $nombre;
     private $saldo;
     public $recarga;
+    public $recargasPosibles = [150, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 2000, 2500, 3000, 3500, 4000];
 
     function nuevaTarjeta($id, $nombre)
     {
@@ -17,18 +18,22 @@ class Tarjeta
         return "Nueva tarjeta creada. ID: " + $this->id + "Titular: " + $this->nombre;
     }
 
-    function pagarBoleto($id, $precio)
+    function recargarSaldo($id, $recarga, $recargasPosibles)
     {
-        $this->saldo = $this->saldo - $precio;
+        if(($this->saldo + $recarga) > 6600){
+            return "El saldo maximo de $6600 ha sido superado";
+        }
+        
+        else{
+            for($i = 0; $i < 23; $i++){
+                if ($recarga == $recargasPosibles[$i]) {
+                    $this->saldo = $this->saldo + $recarga;
+                    return "Recarga completa. Nuevo saldo: " + $this->saldo;
+                }
+            }
+            return "No se reconoce la cantidad a recargar";
+        }
 
-        return "Boleto Pagado. Nuevo saldo: " + $this->saldo;
-    }
-
-    function recargarSaldo($id, $recarga)
-    {
-        $this->saldo = $this->saldo + $recarga;
-
-        return "Recarga completa. Nuevo saldo: " + $this->saldo;
     }
 
 }
