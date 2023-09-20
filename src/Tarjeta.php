c1ac9b6676a0af11dd1936bd1de2519f049dcfd1
TomasGruning <tomasgruning@gmail.com>
2023-09-19 23:49:21 -0300

    Fix recargaSaldo and tests

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -10,7 +10,6 @@ class Tarjeta
     public $saldoMaximo = 6600;
     public $recargasPosibles = [150, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 2000, 2500, 3000, 3500, 4000];
     public $historialBoletos = [];
-    public $excedente;
     public $cargaPendiente = 0;
     
     public function __construct($id, $saldo = 0)
@@ -22,14 +21,13 @@ class Tarjeta
     public function recargarSaldo($recarga)
     {
         if(($this->saldo + $recarga) > $this->saldoMaximo){
-            $this->excedente = ($this->saldo + $recarga) - $this->saldoMaximo;
+            $this->cargaPendiente = ($this->saldo + $recarga) - $this->saldoMaximo;
             $this->saldo = $this->saldoMaximo;
-            $this->cargaPendiente = $this->cargaPendiente + $this->excedente;
 
             return $this->saldo;
         }
         
-        else{
+        else if($this->cargaPendiente == 0){
             for($i = 0; $i < 23; $i++){
                 if ($recarga == $this->recargasPosibles[$i]) {
                     $this->saldo = $this->saldo + $recarga;
@@ -37,6 +35,12 @@ class Tarjeta
                 }
             }
             return false;
+        }
+
+        else{
+            $this->saldo = $this->saldo + $this->cargaPendiente;
+            $this->cargaPendiente = 0;
+            return $this->saldo;
         }
 
     }
--- tests/TarjetasTest.php
+++ tests/TarjetasTest.php
@@ -32,9 +32,9 @@ class TarjetasTest extends TestCase
         $cole = new Colectivo(103);
         $tarjeta = new Tarjeta(uniqid(), 6500);
 
-        $this->assertEquals($tarjeta->recargarSaldo(200), 6600);
-        $this->assertEquals($tarjeta->cargaPendiente, 100);
-        $this->assertEquals($cole->pagarCon($tarjeta), 6580);
+        $this->assertEquals($tarjeta->recargarSaldo(300), 6600);
+        $this->assertEquals($tarjeta->cargaPendiente, 200);
+        $this->assertEquals($cole->pagarCon($tarjeta), 6600);
         $this->assertEquals($tarjeta->cargaPendiente, 80);
     }
 }
